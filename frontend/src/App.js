3be5807dd31812a291a7213cc280a2bf307fc657
milleseseau <leojimlavi@gmail.com>
2021-12-03 00:51:40 -0500

    Using React Mapbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+
 function App() {
-	return <div className="App">Hello</div>;
+	const [viewport, setViewport] = useState({
+		width: "100vw",
+		height: "100vh",
+		latitude: 46,
+		longitude: 17,
+		zoom: 4,
+	});
+	return (
+		<div className="App">
+			<ReactMapGL
+				{...viewport}
+				mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+				onViewportChange={(nextViewport) => setViewport(nextViewport)}
+			>
+				<Marker
+					latitude={37.78}
+					longitude={-122.41}
+					offsetLeft={-20}
+					offsetTop={-10}
+				>
+					<div>You are here</div>
+				</Marker>
+			</ReactMapGL>
+		</div>
+	);
 }
 
 export default App;
