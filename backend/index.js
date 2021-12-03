61353007831f9a9fcffed4efcd9d78783c5492b9
milleseseau <leojimlavi@gmail.com>
2021-12-02 22:19:37 -0500

    creating Node.js REST API

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const pinRoute = require("./routes/pins");
 
 dotenv.config();
+
+app.use(express.json());
 
 mongoose
 	.connect(process.env.MONGO_URL, {
@@ -14,6 +17,8 @@ mongoose
 		console.log("Connected to MongoDB");
 	})
 	.catch((err) => console.log(err));
+
+app.use("/api/pins", pinRoute);
 
 app.listen(8800, () => {
 	console.log("Backend server is running");
