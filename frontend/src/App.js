cceb092d3f0db8a849380c27df5c66d5ded94f5e
milleseseau <leojimlavi@gmail.com>
2021-12-04 00:14:19 -0500

    React Mapbox adding new Location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ function App() {
 	const currentUser = "john";
 	const [pins, setPins] = useState([]);
 	const [currentPlaceId, setCurrentPlaceId] = useState(null);
+	const [newPlace, setNewPlace] = useState(null);
 	const [viewport, setViewport] = useState({
 		width: "100vw",
 		height: "100vh",
@@ -29,8 +30,17 @@ function App() {
 		getPins();
 	}, []);
 
-	const handleMarkerClick = (id) => {
+	const handleMarkerClick = (id, lat, long) => {
 		setCurrentPlaceId(id);
+		setViewport({ ...viewport, latitude: lat, longitude: long });
+	};
+
+	const handleAddClick = (e) => {
+		const [long, lat] = e.lngLat;
+		setNewPlace({
+			lat,
+			long,
+		});
 	};
 
 	return (
@@ -40,6 +50,8 @@ function App() {
 				mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
 				onViewportChange={(nextViewport) => setViewport(nextViewport)}
 				mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
+				onDblClick={handleAddClick}
+				transitionDuration="200"
 			>
 				{pins.map((p) => (
 					<>
@@ -55,7 +67,7 @@ function App() {
 									color: p.username === currentUser ? "tomato" : "slateblue",
 									cursor: "pointer",
 								}}
-								onClick={() => handleMarkerClick(p._id)}
+								onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
 							/>
 						</Marker>
 						;
@@ -91,6 +103,37 @@ function App() {
 						)}
 					</>
 				))}
+
+				{newPlace && (
+					<Popup
+						latitude={newPlace.lat}
+						longitude={newPlace.long}
+						closeButton={true}
+						closeOnClick={false}
+						anchor="left"
+						onClose={() => setNewPlace(null)}
+					>
+						<div>
+							<form>
+								<label>Title</label>
+								<input placeholder="Enter a title" />
+								<label>Review</label>
+								<textarea placeholder="Say us something about this place." />
+								<label>Rating</label>
+								<select>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+								</select>
+								<button className="submitButton" type="submit">
+									Add Pin
+								</button>
+							</form>
+						</div>
+					</Popup>
+				)}
 			</ReactMapGL>
 		</div>
 	);
